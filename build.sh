eba292665a3a1687fe4231610ca1ca5269889716
ufrs12 <ufrs12@yandex.ru>
2024-04-16 08:56:16 +0300

    1

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 git clone --depth=1 --branch=v24.02 https://github.com/armbian/build
-sh ./build/compile.sh PREFER_DOCKER=no INCLUDE_HOME_DIR=yes EXPERT=yes BUILD_MINIMAL=no BUILD_DESKTOP=no BRANCH=legacy RELEASE=bookworm BOARD=orangepi3-lts KERNEL_CONFIGURE=no 
+
+./build/compile.sh PREFER_DOCKER=no INCLUDE_HOME_DIR=yes EXPERT=yes BUILD_MINIMAL=no BUILD_DESKTOP=no BRANCH=legacy RELEASE=bookworm BOARD=orangepi3-lts KERNEL_CONFIGURE=no REPOSITORY_INSTALL="nginx"
