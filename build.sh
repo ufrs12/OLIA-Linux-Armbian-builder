1049f95f2b59ee438a1fd084745beafad6e46625
ufrs12 <ufrs12@yandex.ru>
2024-04-18 13:48:25 +0300

    1

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ git clone --depth=1 --branch=main https://github.com/ufrs12/build
 # 2. Add packages
 echo 'mc' >> ./build/config/cli/common/main/packages
 echo 'nginx' >> ./build/config/cli/common/main/packages
+echo 'unzip' >> ./build/config/cli/common/main/packages
 
 # 3. Run Armbian build with options first time
 ./build/compile.sh  PREFER_DOCKER=no \
